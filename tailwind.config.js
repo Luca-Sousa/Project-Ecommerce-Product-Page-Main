5f7883a361cb12a565ed014d88fcdc3ac0d9b15c
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-07-29 16:34:42 -0300

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        colorOrange: 'hsl(26, 100%, 55%)',
+        colorPaleOrange: 'hsl(25, 100%, 94%)',
+        colorVeryDarkBlue: 'hsl(220, 13%, 13%)',
+        colorDarkGrayishBlue:'hsl(219, 9%, 45%)',
+        colorGrayishBlue: 'hsl(220, 14%, 75%)',
+        colorLightGrayishBlue: 'hsl(223, 64%, 98%)',
+        colorWhite: 'hsl(0, 0%, 100%)',
+        // (with 75% opacity for lightbox background)
+        colorBlack : 'hsl(0, 0%, 0%)',
+      },
+      fontFamily: {
+        KumbhSans: "Kumbh Sans"
+      }
+    },
+  },
+  plugins: [],
+}
+
